2c0b66978ac54deb972fff285ecb48826de51d52
Alexey Voznyuk <me@swizard.info>
2014-07-25 19:58:05 +0400

    environment implementation

--- gcc/compiler.lisp
+++ gcc/compiler.lisp
@@ -1,30 +1,43 @@
 
 (in-package :gcc)
 
-(defclass translate-state ()
-  ((known-procs :initform (make-hash-table :test 'eq) :reader known-procs)))
-
 (defun translate (ast)
-  (translate-walker ast (make-instance 'translate-state)))
+  (translate-walker ast '()))
 
-(defun translate-walker (ast state)
+(defun translate-walker (ast environment)
   (flet ((assume (form) (return-from translate-walker form)))
     (macrolet ((with-match (() &rest clauses)
                  `(progn ,@(iter (for (pattern action) in clauses)
                                  (collect `(if-match ,pattern ast (assume ,action)))))))
       (with-match ()
-        ((define (?proc-name . ?proc-args) ?forms) (translate-define ?proc-name ?proc-args ?forms state))
-        (?atom (translate-atom ?atom state))))
+        ((define (?proc-name . ?proc-args) ?forms) (translate-define ?proc-name ?proc-args ?forms environment))
+        ((+ ?form-a ?form-b) (translate-+ ?form-a ?form-b environment))
+        (?atom (translate-atom ?atom environment))))
     (error "Invalid AST: ~s" ast)))
 
-(defun translate-define (proc-name proc-args proc-body state)
-  (declare (ignore proc-args))
-  (setf (gethash proc-name (known-procs state)) t)
-  (list (list :label proc-name)
-        (translate-walker proc-body state)
-        (list :rtn)))
+(defun translate-define (proc-name proc-args proc-body environment)
+  `((:label ,proc-name)
+    ,@(translate-walker proc-body (cons proc-args environment))
+    (:rtn)))
                 
-(defun translate-atom (atom state)
-  (declare (ignore state))
-  (assert (typep atom 'integer)) ;; only integers allowed
-  (list (list :ldc atom)))
+(defun translate-atom (atom environment)
+  (etypecase atom
+    (integer (translate-const atom environment))
+    (atom (translate-variable atom environment))))
+
+(defun translate-const (const environment)
+  (declare (ignore environment))
+  (assert (typep const 'integer))
+  `((:ldc ,const)))
+
+(defun translate-variable (var environment)
+  (iter (for n from 0)
+        (for frame in environment)
+        (for i = (position var frame :test 'eq))
+        (when i
+          (return-from translate-variable
+            `((:ld ,n ,i)))))
+  (error "Variable ~s unbound" var))
+
+(defun translate-+ (form-a form-b environment)
+  `(,@(translate-walker form-a environment) ,@(translate-walker form-b environment) (:add)))
