fc5cd7e2aa4845ff822ff0e95c094f0bef79eebb
Alexey Voznyuk <me@swizard.info>
2014-07-26 00:31:55 +0400

    initial letrec support

--- gcc/compiler.lisp
+++ gcc/compiler.lisp
@@ -5,7 +5,7 @@
     (translate (read source))))
 
 (defun translate (ast)
-  (translate-walker ast '() '()))
+  (translate-walker ast '()))
 
 (defmacro with-match ((ast proc) &rest clauses)
   (with-gensyms (assume form)
@@ -13,48 +13,55 @@
        (progn ,@(iter (for (pattern action) in clauses)
                       (collect `(if-match ,pattern ,ast (,assume ,action))))))))
 
-(defun translate-walker (ast env rec-env)
+(defun translate-walker (ast env)
   (declare (optimize (debug 3)))
   (with-match (ast translate-walker)
-    ((let ?bindings ?body) (translate-let ?bindings ?body env rec-env))
-    ((letrec ?bindings ?body) (translate-letrec ?bindings ?body env rec-env))
-    ((+ ?form-a ?form-b) (translate-op :add ?form-a ?form-b env rec-env))
-    ((- ?form-a ?form-b) (translate-op :sub ?form-a ?form-b env rec-env))
-    ((* ?form-a ?form-b) (translate-op :mul ?form-a ?form-b env rec-env))
-    ((/ ?form-a ?form-b) (translate-op :div ?form-a ?form-b env rec-env))
-    ((?proc-name . ?proc-args) (translate-proc-invocation ?proc-name ?proc-args env rec-env))
-    (?atom (translate-atom ?atom env rec-env)))
+    ((let ?bindings ?body) (translate-let ?bindings ?body env nil))
+    ((letrec ?bindings ?body) (translate-letrec ?bindings ?body env))
+    ((+ ?form-a ?form-b) (translate-op :add ?form-a ?form-b env))
+    ((- ?form-a ?form-b) (translate-op :sub ?form-a ?form-b env))
+    ((* ?form-a ?form-b) (translate-op :mul ?form-a ?form-b env))
+    ((/ ?form-a ?form-b) (translate-op :div ?form-a ?form-b env))
+    ((?proc-name . ?proc-args) (translate-proc-invocation ?proc-name ?proc-args env))
+    (?atom (translate-atom ?atom env)))
   (error "Invalid AST: ~s" ast))
 
-(defun translate-letrec (binding-forms let-body env rec-env)
-  (translate-let binding-forms let-body env rec-env))
+(defun parse-binding (binding-form)
+  (with-match (binding-form parse-binding)
+    ((?binding (lambda ?proc-args ?proc-body)) (list ?binding ?proc-args ?proc-body))))
+
+(defun translate-letrec (binding-forms let-body env)
+  (declare (optimize (debug 3)))
+  (iter (for binding-form in binding-forms)
+        (for (binding proc-args proc-body) = (parse-binding binding-form))
+        (collect binding into bindings)
+        (finally
+         (return
+           (translate-let binding-forms let-body env bindings)))))
 
 (defun translate-let (binding-forms let-body env rec-env)
   (declare (optimize (debug 3)))
-  (flet ((parse-binding (binding-form)
-           (with-match (binding-form parse-binding)
-             ((?binding (lambda ?proc-args ?proc-body)) (list ?binding ?proc-args ?proc-body)))))
-    (iter (for binding-form in binding-forms)
-          (for (binding proc-args proc-body) = (parse-binding binding-form))
-          (collect binding into bindings)
-          (collect (translate-walker proc-body (cons proc-args env) rec-env) into codes)
-          (finally
-           (return
-             (append (translate-walker let-body (cons bindings env) rec-env)
-                     (iter outer
-                           (for proc-label in bindings)
-                           (for proc-code in codes)
-                           (collect `(:label ,proc-label))
-                           (iter (for form in proc-code) (in outer (collect form))))
-                     '((:rtn))))))))
+  (iter (for binding-form in binding-forms)
+        (for (binding proc-args proc-body) = (parse-binding binding-form))
+        (collect binding into bindings)
+        (collect (translate-walker proc-body (cons (append proc-args rec-env) env)) into codes)
+        (finally
+         (return
+           (append (translate-walker let-body bindings)
+                   (iter outer
+                         (for proc-label in bindings)
+                         (for proc-code in codes)
+                         (collect `(:label ,proc-label))
+                         (iter (for form in proc-code) (in outer (collect form))))
+                   '((:rtn)))))))
                 
-(defun translate-atom (atom env rec-env)
+(defun translate-atom (atom env)
   (etypecase atom
-    (integer (translate-const atom env rec-env))
-    (atom (translate-variable atom env rec-env))))
+    (integer (translate-const atom env))
+    (atom (translate-variable atom env))))
 
-(defun translate-const (const env rec-env)
-  (declare (ignore env rec-env))
+(defun translate-const (const env)
+  (declare (ignore env))
   (assert (typep const 'integer))
   `((:ldc ,const)))
 
@@ -66,17 +73,18 @@
           (return-from locate-within-env (values n i))))
   (error "Variable ~s unbound" bind))
 
-(defun translate-variable (var env rec-env)
-  (declare (ignore rec-env))
+(defun translate-variable (var env)
+  (declare (ignore))
   (multiple-value-bind (n i) (locate-within-env var env)
     `((:ld ,n ,i))))
 
-(defun translate-op (op form-a form-b env rec-env)
-  `(,@(translate-walker form-b env rec-env) ,@(translate-walker form-a env rec-env) (,op)))
+(defun translate-op (op form-a form-b env)
+  `(,@(translate-walker form-b env) ,@(translate-walker form-a env) (,op)))
 
-(defun translate-proc-invocation (proc-name proc-args env rec-env)
-  (multiple-value-bind (n i) (locate-within-env proc-name env)
-    `(,@(apply #'append (mapcar (lambda (form) (translate-walker form env rec-env)) (reverse proc-args)))
-      (:ld ,n ,i)
+(defun translate-proc-invocation (proc-name proc-args env)
+  (declare (optimize (debug 3)))
+  (multiple-value-bind (n i) (ignore-errors (locate-within-env proc-name env))
+    `(,@(apply #'append (mapcar (lambda (form) (translate-walker form env)) (reverse proc-args)))
+      ,@(if (and n i) `((:ld ,n ,i)) `((:ldf ,proc-name)))
       (:ap ,(length proc-args)))))
   
