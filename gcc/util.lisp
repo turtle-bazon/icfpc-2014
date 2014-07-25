ca2cbbfcb579e5b2a786c98f4c58913034bf574c
Gleb Golubitsky <sectoid@gnolltech.org>
2014-07-26 00:48:19 +0300

    [Util] Make pretty print use *standard-output* by default
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- gcc/util.lisp
+++ gcc/util.lisp
@@ -1,6 +1,6 @@
 (in-package :gcc)
 
-(defun pretty-print-gcc (gcc &optional (stream t))
+(defun pretty-print-gcc (gcc &optional (stream *standard-output*))
   (iter (for form in gcc)
         (bind (((op &rest args) form))
           (case op
