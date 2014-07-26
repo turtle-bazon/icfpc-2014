4b6548c1612b85090076bda9a1b95c36ee47d8ff
Stanislav Ivankin <lessgrep@gmail.com>
2014-07-26 19:18:47 +0900

    FIlling up l-man interface

--- gcc/l-man.lisp
+++ gcc/l-man.lisp
@@ -8,8 +8,20 @@
 (deflib/gcc l-man-coord (l)
   (car (cdr l)))
 
+(deflib/gcc nth (n lst)
+  (if (= n 0) (car lst) (nth (- n 1) (cdr lst))))
+
+(deflib/gcc map-size (map x y)
+  (if (integerp (cdr map))
+      (if (= x 0)
+	  (map-size (car map) (+ 1 x) y)
+	  (cons x y))
+      (if (= x 0)
+	  (map-size (cdr map) x (+ 1 y))
+	  (map-size (cdr map) (+ 1 x) y))))
+
 (deflib/gcc move (ai-state w-state)
-  (if (= (car (l-man-coord (l-man-state w-state))) 11)
+  (if (= (car (map-size (car w-state) 0 0)) 23)
       (cons ai-state :left)
       (cons ai-state :rigth)))
 
