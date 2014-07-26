a60fe5bbe9710fcd9f09fd935269afda09771e73
Stanislav Ivankin <lessgrep@gmail.com>
2014-07-26 14:47:53 +0900

    * gcc/util.lisp (pretty-print-gcc): Padding switch key

--- gcc/util.lisp
+++ gcc/util.lisp
@@ -1,6 +1,7 @@
 (in-package :gcc)
 
-(defun pretty-print-gcc (gcc &key (stream *standard-output*) (minimize nil))
+(defun pretty-print-gcc (gcc &key (stream *standard-output*) (minimize nil)
+			       (pad t))
   (iter (for form in gcc)
         (let ((unlabeled (eq 'unlabeled (first form))))
 	  (bind (((op &rest args) (if unlabeled
@@ -11,4 +12,5 @@
 			(format stream (if unlabeled
 					   ";~a:~%"
 					   "~a:~%") (first args))))
-	      (t (format stream "    ~a~{ ~a~}~%" op args)))))))
+	      (t (format stream "~a~a~{ ~a~}~%"
+			 (if pad "    " "") op args)))))))
