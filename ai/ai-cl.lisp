364ff58b25e7acde802313d5e3f8dc7f9eff8da2
Alexey Voznyuk <me@swizard.info>
2014-07-27 15:27:22 +0400

    il-foldl and locate-objects tools

--- ai/ai-cl.lisp
+++ ai/ai-cl.lisp
@@ -1,3 +1,26 @@
 
 (in-package :ai)
 
+(defun il-foldl (proc accumulator il-list)
+  (let ((next-accumulator (funcall proc (car il-list) accumulator))
+        (rest (cdr il-list)))
+    (if (integerp rest)
+        next-accumulator
+        (il-foldl proc next-accumulator rest))))
+
+(defun locate-objects (object map)
+  (cdr (il-foldl (lambda (row acc)
+                   (let ((y (car acc)) (objects (cdr acc)))
+                     (cons (+ y 1)
+                           (cdr (il-foldl (lambda (cell acc)
+                                            (let ((x (car acc)) (objects (cdr acc)))
+                                              (cons (+ x 1)
+                                                    (if (= cell object)
+                                                        (cons (cons x y) objects)
+                                                        objects))))
+                                          (cons 0 objects)
+                                          row)))))
+                 (cons 0 0)
+                 map)))
+
+                 
