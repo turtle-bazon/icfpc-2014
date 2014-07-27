d4e7021cc164be07e9b857be4e8fbd9955791c18
Alexey Voznyuk <me@swizard.info>
2014-07-27 19:36:13 +0400

    fully working bumper-ai from lightning round ported to ILISP

--- ilisp/examples/bumper-ai.lisp
+++ ilisp/examples/bumper-ai.lisp
@@ -3,11 +3,39 @@
 
 (cl:in-package :ilisp-bumper-ai)
 
-(defun il-nth (n lst)
-  (if (= n 0) (car lst) (il-nth (- n 1) (cdr lst))))
+(defun nth (n lst)
+  (if (= n 0) (car lst) (nth (- n 1) (cdr lst))))
+
+(defun call-with-world (ai-state map lman ghosts fruits)
+  (letrec ((at-coords (lambda (x y) (nth x (nth y map))))
+           (can-walk (lambda (x y)
+                       (if (= x (car ai-state))
+                           (if (= y (cdr ai-state))
+                               0
+                               (at-coords x y))
+                           (at-coords x y)))))
+    (let ((up (can-walk (car lman) (- (cdr lman) 1)))
+          (rg (can-walk (+ (car lman) 1) (cdr lman)))
+          (dn (can-walk (car lman) (+ (cdr lman) 1)))
+          (lf (can-walk (- (car lman) 1) (cdr lman))))
+      (let ((decide (lambda (direction)
+                      (cons (cons (car lman) (cdr lman)) direction))))
+        (if (= lf 0)
+            (if (= up 0)
+                (if (= rg 0)
+                    (if (= dn 0)
+                        (decide :left)
+                        (decide :down))
+                    (decide :rigth))
+                (decide :up))
+            (decide :left))))))
 
 (defun step (ai-state world-state)
-  (cons ai-state 3))
+  (call-with-world ai-state
+                   (car world-state)
+                   (car (cdr (car (cdr world-state))))
+                   (car (cdr (cdr world-state)))
+                   (cdr (cdr (cdr world-state)))))
 
 (defun init (world-initial-state ghosts-statuses)
   (cons (cons 0 0) step))
