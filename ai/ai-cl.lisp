f455fdf267c003d31d52dddf226b14cad4d5f54b
Alexey Voznyuk <me@swizard.info>
2014-07-27 16:02:22 +0400

    locating nearest objects

--- ai/ai-cl.lisp
+++ ai/ai-cl.lisp
@@ -23,4 +23,25 @@
                  (cons 0 0)
                  map)))
 
-                 
+(defun sq-dist (x-a y-a x-b y-b)
+  (let ((diff-x (- x-a x-b)) (diff-y (- y-a y-b)))
+    (+ (* diff-x diff-x) (* diff-y diff-y))))
+
+(defun pop-nearest-object (my-x my-y objects)
+  (declare (optimize (debug 3)))
+  (il-foldl (lambda (object acc)
+              (let ((nearest-object (car acc))
+                    (nearest-sq-dist (cadr acc))
+                    (rest-objects (cddr acc))
+                    (current-sq-dist (sq-dist my-x my-y (car object) (cdr object))))
+                (if (or (integerp nearest-object)
+                        (< current-sq-dist nearest-sq-dist))
+                    (cons object
+                          (cons current-sq-dist
+                                (if (integerp nearest-object)
+                                    rest-objects
+                                    (cons nearest-object rest-objects))))
+                    (cons nearest-object (cons nearest-sq-dist (cons object rest-objects))))))
+            (cons 0 (cons 0 0))
+            objects))
+
